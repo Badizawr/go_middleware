fd093ff712ae89569c32ae7a2aa1706091a683f6
Badizawr <vicodin-13@ya.ru>
2024-08-28 16:57:00 +0500

    create first logic on server

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
 	fmt.Println("Server running")
+
+	s := echo.New()
+
+	s.GET("/status", Handler)
+
+	err := s.Start(":8050")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Handler(ctx echo.Context) error {
+	d := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	dur := time.Until(d)
+
+	s := fmt.Sprintf("Количество дней до Января 2025 осталось: %d", int64(dur.Hours())/24)
+
+	err := ctx.String(http.StatusOK, s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
