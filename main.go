dd40a9070238201f106171a748d27dd7671d69a9
Badizawr <vicodin-13@ya.ru>
2024-08-29 13:27:08 +0500

    admin response

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 
 	s := echo.New()
 
+	s.Use(MW)
+
 	s.GET("/status", Handler)
 
 	err := s.Start(":8050")
@@ -34,4 +36,21 @@ func Handler(ctx echo.Context) error {
 		return err
 	}
 	return nil
+}
+
+func MW(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		val := ctx.Request().Header.Get("User-Role")
+
+		if val == "admin" {
+			log.Println("red button user detected")
+		}
+
+		err := next(ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
 }
